4d6e9b734a9f61dc9dc32f8e1fcf374860f70058
aswindevmp <aswin.dev@factweavers.com>
2024-04-09 14:26:36 +0530

    removed delete api.

--- demo-CrewAi/main.py
+++ demo-CrewAi/main.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 from crud.create import CrudCreator
 from crud.read import CrudReader
 from crud.update import CrudUpdate
-from crud.delete import CrudDelete
 
 app = FastAPI()
 load_dotenv()
@@ -30,12 +29,6 @@ async def update_tool(tool_id: str, tool: dict):
     crudupdate = CrudUpdate()
     return await crudupdate.update_tool(tool_id, tool)
 
-#Delete Tool
-@app.delete("/tool/{tool_id}")
-async def delete_tool(tool_id: str):
-    cruddelete = CrudDelete()
-    return await cruddelete.delete_tool(tool_id)
-
 """.........................AGENTS.........................."""
 
 # Create Agent
@@ -55,12 +48,6 @@ async def read_agents(filter_dict: dict = None):
 async def update_agent(agent_id: str, agent: dict):
     crudupdate = CrudUpdate()
     return await crudupdate.update_agent(agent_id, agent)
-
-#Delete Agent
-@app.delete("/agent/{agent_id}")
-async def delete_agent(agent_id: str):
-    cruddelete = CrudDelete()
-    return await cruddelete.delete_agent(agent_id)
 
 """.........................TASKS.........................."""
 
@@ -82,12 +69,6 @@ async def update_task(task_id: str, task: dict):
     crudupdate = CrudUpdate()
     return await crudupdate.update_task(task_id,task)
 
-#Delete Task
-@app.delete("/task/{task_id}")
-async def delete_task(task_id: str):
-    cruddelete = CrudDelete()
-    return await cruddelete.delete_task(task_id)
-
 """.........................CREWS.........................."""
 
 #Create Crew
@@ -108,20 +89,4 @@ async def update_crew(crew_id: str, crew: dict):
     crudupdate = CrudUpdate()
     return await crudupdate.update_crew(crew_id,crew)
 
-#Delete Crew
-@app.delete("/crew/{crew_id}")
-async def delete_crew(crew_id: str):
-    cruddelete = CrudDelete()
-    return await cruddelete.delete_crew(crew_id)
-
-
-
-# @app.post("/upload")
-# async def upload(file:UploadFile = File(...)):
-#     file_ext = file.filename.split(".").pop()  #Eg :- png, jpeg
-#     file_name = token_hex(10)
-#     file_path = f"{file_name}.{file_ext}"
-#     with open(file_path,"wb") as f:
-#         content = await file.read()
-#         f.write(content)
-#     return {"success":True, "file_path": file_path, "message": "File uploaded successfully"}    
+   
